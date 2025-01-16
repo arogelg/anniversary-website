e5f240325965901c8ef04673a0de96ed7aa43779
Alejandro Rogel <alex_rogelgarcia1@baylor.edu>
2025-01-16 16:38:34 -0600

    General changes for Gallery with the new Slider

--- script.js
+++ script.js
@@ -32,92 +32,55 @@ document.addEventListener('click', (event) => {
   }
 });
 
-// Params
-let mainSliderSelector = '.main-slider',
-    navSliderSelector = '.nav-slider',
-    interleaveOffset = 0.5;
-
-// Main Slider
-let mainSliderOptions = {
-      loop: true,
-      speed:1000,
-      autoplay:{
-        delay:3000
-      },
-      loopAdditionalSlides: 10,
-      grabCursor: true,
-      watchSlidesProgress: true,
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      on: {
-        init: function(){
-          this.autoplay.stop();
-        },
-        imagesReady: function(){
-          this.el.classList.remove('loading');
-          this.autoplay.start();
-        },
-        slideChangeTransitionEnd: function(){
-          let swiper = this,
-              captions = swiper.el.querySelectorAll('.caption');
-          for (let i = 0; i < captions.length; ++i) {
-            captions[i].classList.remove('show');
-          }
-          swiper.slides[swiper.activeIndex].querySelector('.caption').classList.add('show');
-        },
-        progress: function(){
-          let swiper = this;
-          for (let i = 0; i < swiper.slides.length; i++) {
-            let slideProgress = swiper.slides[i].progress,
-                innerOffset = swiper.width * interleaveOffset,
-                innerTranslate = slideProgress * innerOffset;
-           
-            swiper.slides[i].querySelector(".slide-bgimg").style.transform =
-              "translateX(" + innerTranslate + "px)";
-          }
-        },
-        touchStart: function() {
-          let swiper = this;
-          for (let i = 0; i < swiper.slides.length; i++) {
-            swiper.slides[i].style.transition = "";
-          }
-        },
-        setTransition: function(speed) {
-          let swiper = this;
-          for (let i = 0; i < swiper.slides.length; i++) {
-            swiper.slides[i].style.transition = speed + "ms";
-            swiper.slides[i].querySelector(".slide-bgimg").style.transition =
-              speed + "ms";
-          }
-        }
+const mainSlider = new Swiper('.main-slider', {
+  loop: true,
+  speed: 1000,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  thumbs: {
+    swiper: null, // Will be set later
+  },
+  on: {
+    imagesReady: function () {
+      this.el.classList.remove('loading');
+    },
+    slideChangeTransitionEnd: function () {
+      let activeSlide = this.slides[this.activeIndex];
+      let caption = activeSlide.querySelector('.caption');
+      if (caption) {
+        caption.classList.add('show');
       }
-    };
-let mainSlider = new Swiper(mainSliderSelector, mainSliderOptions);
+    },
+    slideChange: function () {
+      let captions = this.el.querySelectorAll('.caption');
+      captions.forEach(caption => caption.classList.remove('show'));
+    },
+  },
+});
 
-// Navigation Slider
-let navSliderOptions = {
-      loop: true,
-      loopAdditionalSlides: 10,
-      speed:1000,
-      spaceBetween: 5,
-      slidesPerView: 5,
-      centeredSlides : true,
-      touchRatio: 0.2,
-      slideToClickedSlide: true,
-      direction: 'vertical',
-      on: {
-        imagesReady: function(){
-          this.el.classList.remove('loading');
-        },
-        click: function(){
-          mainSlider.autoplay.stop();
-        }
-      }
-    };
-let navSlider = new Swiper(navSliderSelector, navSliderOptions);
+// Thumbnail Navigation Slider
+const navSlider = new Swiper('.nav-slider', {
+  loop: true,
+  spaceBetween: 10,
+  slidesPerView: 3,
+  direction: 'vertical',
+  centeredSlides: true,
+  slideToClickedSlide: true,
+  on: {
+    imagesReady: function () {
+      this.el.classList.remove('loading');
+    },
+    click: function () {
+      mainSlider.autoplay.stop(); // Stops autoplay when clicking thumbnails
+    },
+  },
+});
 
-// Matching sliders
-mainSlider.controller.control = navSlider;
-navSlider.controller.control = mainSlider;
+// Connect Navigation Slider to Main Slider
+mainSlider.thumbs.swiper = navSlider;
